b9f488b6a3dabd45ce9d3e356c3125bb197b836b
lmittmann <lmittmann@users.noreply.github.com>
2022-02-25 13:05:14 +0100

    UserStats(...): reduced complexity
    * removed unused `MarshalJSON`

--- get_user_stats.go
+++ get_user_stats.go
@@ -1,7 +1,7 @@
-//go:generate gencodec -type UserStatsResponse -field-override userStatsResponseMarshaling -out get_user_stats.gen.go
 package flashbots
 
 import (
+	"encoding/json"
 	"math/big"
 
 	"github.com/ethereum/go-ethereum/common/hexutil"
@@ -9,23 +9,54 @@ import (
 )
 
 type UserStatsResponse struct {
-	IsHighPriority       bool     `json:"is_high_priority"`
-	AllTimeMinerPayments *big.Int `json:"all_time_miner_payments"`
-	AllTimeGasSimulated  *big.Int `json:"all_time_gas_simulated"`
-	Last7dMinerPayments  *big.Int `json:"last_7d_miner_payments"`
-	Last7dGasSimulated   *big.Int `json:"last_7d_gas_simulated"`
-	Last1dMinerPayments  *big.Int `json:"last_1d_miner_payments"`
-	Last1dGasSimulated   *big.Int `json:"last_1d_gas_simulated"`
+	IsHighPriority       bool     // True if the searcher has an high enough reputation to be in the high priority queue.
+	AllTimeMinerPayments *big.Int // Total amount paid to miners over all time.
+	AllTimeGasSimulated  *big.Int // Total amount of gas simulated across all bundles submitted to the relay.
+	Last7dMinerPayments  *big.Int // Total amount paid to miners over the last 7 days.
+	Last7dGasSimulated   *big.Int // Total amount of gas simulated across all bundles submitted to the relay in the last 7 days.
+	Last1dMinerPayments  *big.Int // Total amount paid to miners over the last day.
+	Last1dGasSimulated   *big.Int // Total amount of gas simulated across all bundles submitted to the relay in the last day.
 }
 
-type userStatsResponseMarshaling struct {
-	IsHighPriority       bool
-	AllTimeMinerPayments *strBigint
-	AllTimeGasSimulated  *strBigint
-	Last7dMinerPayments  *strBigint
-	Last7dGasSimulated   *strBigint
-	Last1dMinerPayments  *strBigint
-	Last1dGasSimulated   *strBigint
+type userStatsResponse struct {
+	IsHighPriority       *bool      `json:"is_high_priority"`
+	AllTimeMinerPayments *strBigint `json:"all_time_miner_payments"`
+	AllTimeGasSimulated  *strBigint `json:"all_time_gas_simulated"`
+	Last7dMinerPayments  *strBigint `json:"last_7d_miner_payments"`
+	Last7dGasSimulated   *strBigint `json:"last_7d_gas_simulated"`
+	Last1dMinerPayments  *strBigint `json:"last_1d_miner_payments"`
+	Last1dGasSimulated   *strBigint `json:"last_1d_gas_simulated"`
+}
+
+// UnmarshalJSON unmarshals from JSON.
+func (u *UserStatsResponse) UnmarshalJSON(input []byte) error {
+	var dec userStatsResponse
+	if err := json.Unmarshal(input, &dec); err != nil {
+		return err
+	}
+
+	if dec.IsHighPriority != nil {
+		u.IsHighPriority = *dec.IsHighPriority
+	}
+	if dec.AllTimeMinerPayments != nil {
+		u.AllTimeMinerPayments = (*big.Int)(dec.AllTimeMinerPayments)
+	}
+	if dec.AllTimeGasSimulated != nil {
+		u.AllTimeGasSimulated = (*big.Int)(dec.AllTimeGasSimulated)
+	}
+	if dec.Last7dMinerPayments != nil {
+		u.Last7dMinerPayments = (*big.Int)(dec.Last7dMinerPayments)
+	}
+	if dec.Last7dGasSimulated != nil {
+		u.Last7dGasSimulated = (*big.Int)(dec.Last7dGasSimulated)
+	}
+	if dec.Last1dMinerPayments != nil {
+		u.Last1dMinerPayments = (*big.Int)(dec.Last1dMinerPayments)
+	}
+	if dec.Last1dGasSimulated != nil {
+		u.Last1dGasSimulated = (*big.Int)(dec.Last1dGasSimulated)
+	}
+	return nil
 }
 
 // UserStats requests the users Flashbots relay stats. The given block number
