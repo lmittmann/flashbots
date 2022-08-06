89c1ef9bf23b69dbd727e07bbaecbc63af804b2b
lmittmann <lmittmann@users.noreply.github.com>
2022-08-06 09:26:47 +0200

    refactored private tx

--- private_tx.go
+++ private_tx.go
@@ -76,3 +76,43 @@ func (f *sendPrivateTxFactory) HandleResponse(elem rpc.BatchElem) error {
 	}
 	return nil
 }
+
+type cancelPrivateTxRequest struct {
+	TxHash common.Hash `json:"txHash"`
+}
+
+// CancelPrivateTx stops the private transactions with the given hash
+// from being submitted for future blocks by the Flashbots relay.
+func CancelPrivateTx(hash common.Hash) core.CallerFactory[bool] {
+	return &cancelPrivateTxFactory{hash: hash}
+}
+
+type cancelPrivateTxFactory struct {
+	// args
+	hash common.Hash
+
+	// returns
+	returns *bool
+}
+
+func (f *cancelPrivateTxFactory) Returns(success *bool) core.Caller {
+	f.returns = success
+	return f
+}
+
+func (f *cancelPrivateTxFactory) CreateRequest() (rpc.BatchElem, error) {
+	return rpc.BatchElem{
+		Method: "eth_cancelPrivateTransaction",
+		Args: []any{&cancelPrivateTxRequest{
+			TxHash: f.hash,
+		}},
+		Result: &f.returns,
+	}, nil
+}
+
+func (f *cancelPrivateTxFactory) HandleResponse(elem rpc.BatchElem) error {
+	if err := elem.Error; err != nil {
+		return err
+	}
+	return nil
+}
--- private_tx_test.go
+++ private_tx_test.go
@@ -9,7 +9,7 @@ import (
 	"github.com/lmittmann/w3/rpctest"
 )
 
-func TestSendPrivateTransaction(t *testing.T) {
+func TestSendPrivateTx(t *testing.T) {
 	t.Parallel()
 
 	srv := rpctest.NewFileServer(t, "testdata/send_private_transaction.golden")
@@ -19,7 +19,7 @@ func TestSendPrivateTransaction(t *testing.T) {
 	defer client.Close()
 
 	var (
-		sendPrivateTransactionReq = &SendPrivateTxRequest{
+		sendPrivateTxReq = &SendPrivateTxRequest{
 			RawTx:          w3.B("0x00"),
 			MaxBlockNumber: big.NewInt(9_999_999),
 			Fast:           true,
@@ -29,11 +29,34 @@ func TestSendPrivateTransaction(t *testing.T) {
 	)
 
 	if err := client.Call(
-		SendPrivateTx(sendPrivateTransactionReq).Returns(&hash),
+		SendPrivateTx(sendPrivateTxReq).Returns(&hash),
 	); err != nil {
 		t.Fatalf("Request failed: %v", err)
 	}
 	if wantHash != hash {
 		t.Fatalf("want %v, got %v", wantHash, hash)
+	}
+}
+
+func TestCancelPrivateTx(t *testing.T) {
+	t.Parallel()
+
+	srv := rpctest.NewFileServer(t, "testdata/cancel_private_transaction.golden")
+	defer srv.Close()
+
+	client := w3.MustDial(srv.URL())
+	defer client.Close()
+
+	var (
+		success     bool
+		wantSuccess = true
+	)
+	if err := client.Call(
+		CancelPrivateTx(w3.H("0x45df1bc3de765927b053ec029fc9d15d6321945b23cac0614eb0b5e61f3a2f2a")).Returns(&success),
+	); err != nil {
+		t.Fatalf("Request failed: %v", err)
+	}
+	if wantSuccess != success {
+		t.Fatalf("want %v, got %v", wantSuccess, success)
 	}
 }
