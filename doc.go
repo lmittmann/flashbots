13ea9c33df773ce2deee13c930bcd3d957bf2069
lmittmann <lmittmann@users.noreply.github.com>
2022-08-07 18:53:48 +0200

    added link to doc

--- doc.go
+++ doc.go
@@ -1,7 +1,8 @@
 /*
 Package flashbots implements RPC API bindings for the Flashbots relay and
-mev-geth for use with the [w3 package].
+[mev-geth] for use with the [w3 package].
 
+[mev-geth]: https://github.com/flashbots/mev-geth
 [w3 package]: https://github.com/lmittmann/w3
 */
 package flashbots
