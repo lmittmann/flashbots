47488c438dbc3eab98fb678aa978f5addf7ac3d2
lmittmann <lmittmann@users.noreply.github.com>
2022-09-06 16:55:43 +0200

    improved tests
    * use new `TestCase` from `w3/rpctest`

--- private_tx_test.go
+++ private_tx_test.go
@@ -1,62 +1,39 @@
-package flashbots
+package flashbots_test
 
 import (
 	"math/big"
 	"testing"
 
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/lmittmann/flashbots"
 	"github.com/lmittmann/w3"
 	"github.com/lmittmann/w3/rpctest"
 )
 
 func TestSendPrivateTx(t *testing.T) {
-	t.Parallel()
-
-	srv := rpctest.NewFileServer(t, "testdata/send_private_transaction.golden")
-	defer srv.Close()
-
-	client := w3.MustDial(srv.URL())
-	defer client.Close()
-
-	var (
-		sendPrivateTxReq = &SendPrivateTxRequest{
-			RawTx:          w3.B("0x00"),
-			MaxBlockNumber: big.NewInt(9_999_999),
-			Fast:           true,
-		}
-		hash     common.Hash
-		wantHash = w3.H("0x45df1bc3de765927b053ec029fc9d15d6321945b23cac0614eb0b5e61f3a2f2a")
-	)
-
-	if err := client.Call(
-		SendPrivateTx(sendPrivateTxReq).Returns(&hash),
-	); err != nil {
-		t.Fatalf("Request failed: %v", err)
+	tests := []rpctest.TestCase[common.Hash]{
+		{
+			Golden: "send_private_transaction",
+			Call: flashbots.SendPrivateTx(&flashbots.SendPrivateTxRequest{
+				RawTx:          w3.B("0x00"),
+				MaxBlockNumber: big.NewInt(9_999_999),
+				Fast:           true,
+			}),
+			WantRet: w3.H("0x45df1bc3de765927b053ec029fc9d15d6321945b23cac0614eb0b5e61f3a2f2a"),
+		},
 	}
-	if wantHash != hash {
-		t.Fatalf("want %v, got %v", wantHash, hash)
-	}
+
+	rpctest.RunTestCases(t, tests)
 }
 
 func TestCancelPrivateTx(t *testing.T) {
-	t.Parallel()
-
-	srv := rpctest.NewFileServer(t, "testdata/cancel_private_transaction.golden")
-	defer srv.Close()
-
-	client := w3.MustDial(srv.URL())
-	defer client.Close()
-
-	var (
-		success     bool
-		wantSuccess = true
-	)
-	if err := client.Call(
-		CancelPrivateTx(w3.H("0x45df1bc3de765927b053ec029fc9d15d6321945b23cac0614eb0b5e61f3a2f2a")).Returns(&success),
-	); err != nil {
-		t.Fatalf("Request failed: %v", err)
+	tests := []rpctest.TestCase[bool]{
+		{
+			Golden:  "cancel_private_transaction",
+			Call:    flashbots.CancelPrivateTx(w3.H("0x45df1bc3de765927b053ec029fc9d15d6321945b23cac0614eb0b5e61f3a2f2a")),
+			WantRet: true,
+		},
 	}
-	if wantSuccess != success {
-		t.Fatalf("want %v, got %v", wantSuccess, success)
-	}
+
+	rpctest.RunTestCases(t, tests)
 }
--- stats_test.go
+++ stats_test.go
@@ -1,44 +1,32 @@
-package flashbots
+package flashbots_test
 
 import (
 	"math/big"
 	"testing"
 	"time"
 
-	"github.com/google/go-cmp/cmp"
+	"github.com/lmittmann/flashbots"
 	"github.com/lmittmann/w3"
 	"github.com/lmittmann/w3/rpctest"
 )
 
 func TestBundleStats(t *testing.T) {
-	t.Parallel()
-
-	srv := rpctest.NewFileServer(t, "testdata/get_bundle_stats.golden")
-	defer srv.Close()
-
-	client := w3.MustDial(srv.URL())
-	defer client.Close()
-
-	var (
-		bundleStats     = new(BundleStatsResponse)
-		wantBundleStats = &BundleStatsResponse{
-			IsSimulated:    true,
-			IsSentToMiners: true,
-			IsHighPriority: true,
-			SimulatedAt:    mustParse("2021-08-06T21:36:06.317Z"),
-			SubmittedAt:    mustParse("2021-08-06T21:36:06.250Z"),
-			SentToMinersAt: mustParse("2021-08-06T21:36:06.343Z"),
-		}
-	)
-
-	if err := client.Call(
-		BundleStats(w3.H("0x2228f5d8954ce31dc1601a8ba264dbd401bf1428388ce88238932815c5d6f23f"), big.NewInt(999_999_999)).Returns(bundleStats),
-	); err != nil {
-		t.Fatalf("Request failed: %v", err)
+	tests := []rpctest.TestCase[flashbots.BundleStatsResponse]{
+		{
+			Golden: "get_bundle_stats",
+			Call:   flashbots.BundleStats(w3.H("0x2228f5d8954ce31dc1601a8ba264dbd401bf1428388ce88238932815c5d6f23f"), big.NewInt(999_999_999)),
+			WantRet: flashbots.BundleStatsResponse{
+				IsSimulated:    true,
+				IsSentToMiners: true,
+				IsHighPriority: true,
+				SimulatedAt:    mustParse("2021-08-06T21:36:06.317Z"),
+				SubmittedAt:    mustParse("2021-08-06T21:36:06.250Z"),
+				SentToMinersAt: mustParse("2021-08-06T21:36:06.343Z"),
+			},
+		},
 	}
-	if diff := cmp.Diff(wantBundleStats, bundleStats); diff != "" {
-		t.Fatalf("(-want, +got)\n%s", diff)
-	}
+
+	rpctest.RunTestCases(t, tests)
 }
 
 func mustParse(timeStr string) time.Time {
@@ -50,33 +38,21 @@ func mustParse(timeStr string) time.Time {
 }
 
 func TestUserStats(t *testing.T) {
-	t.Parallel()
-
-	srv := rpctest.NewFileServer(t, "testdata/get_user_stats.golden")
-	defer srv.Close()
-
-	client := w3.MustDial(srv.URL())
-	defer client.Close()
-
-	var (
-		userStats     = new(UserStatsResponse)
-		wantUserStats = &UserStatsResponse{
-			IsHighPriority:       true,
-			AllTimeMinerPayments: w3.I("1280749594841588639"),
-			AllTimeGasSimulated:  w3.I("30049470846"),
-			Last7dMinerPayments:  w3.I("1280749594841588639"),
-			Last7dGasSimulated:   w3.I("30049470846"),
-			Last1dMinerPayments:  w3.I("142305510537954293"),
-			Last1dGasSimulated:   w3.I("2731770076"),
-		}
-	)
-
-	if err := client.Call(UserStats(big.NewInt(999_999_999)).Returns(userStats)); err != nil {
-		t.Fatalf("Request failed: %v", err)
+	tests := []rpctest.TestCase[flashbots.UserStatsResponse]{
+		{
+			Golden: "get_user_stats",
+			Call:   flashbots.UserStats(big.NewInt(999_999_999)),
+			WantRet: flashbots.UserStatsResponse{
+				IsHighPriority:       true,
+				AllTimeMinerPayments: w3.I("1280749594841588639"),
+				AllTimeGasSimulated:  w3.I("30049470846"),
+				Last7dMinerPayments:  w3.I("1280749594841588639"),
+				Last7dGasSimulated:   w3.I("30049470846"),
+				Last1dMinerPayments:  w3.I("142305510537954293"),
+				Last1dGasSimulated:   w3.I("2731770076"),
+			},
+		},
 	}
-	if diff := cmp.Diff(wantUserStats, userStats,
-		cmp.AllowUnexported(big.Int{}),
-	); diff != "" {
-		t.Fatalf("(-want, +got)\n%s", diff)
-	}
+
+	rpctest.RunTestCases(t, tests)
 }
