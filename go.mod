d5b3c7dfba5cfe4e3b3b978ab8fe8ab09e029bf5
krishna <krishnateja262@gmail.com>
2024-09-04 20:21:34 +0530

    replacing from string to uuid data type

--- go.mod
+++ go.mod
@@ -20,6 +20,7 @@ require (
 	github.com/ethereum/c-kzg-4844 v1.0.0 // indirect
 	github.com/go-ole/go-ole v1.3.0 // indirect
 	github.com/google/go-cmp v0.6.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/holiman/uint256 v1.3.1 // indirect
 	github.com/mmcloughlin/addchain v0.4.0 // indirect
--- send_bundle.go
+++ send_bundle.go
@@ -8,6 +8,7 @@ import (
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/rpc"
+	"github.com/google/uuid"
 	"github.com/lmittmann/w3/w3types"
 )
 
@@ -18,7 +19,7 @@ type SendBundleRequest struct {
 	MinTimestamp      uint64             // Minimum Unix Timestamp for which the bundle is valid
 	MaxTimestamp      uint64             // Maximum Unix Timestamp for which the bundle is valid
 	RevertingTxHashes []common.Hash      // List of tx hashes in bundle that are allowed to revert.
-	ReplacementUuid   string             // String, UUID that can be used to cancel/replace this bundle
+	ReplacementUuid   uuid.UUID          // UUID that can be used to cancel/replace this bundle
 }
 
 type sendBundleRequest struct {
@@ -27,7 +28,7 @@ type sendBundleRequest struct {
 	MinTimestamp      uint64          `json:"minTimestamp,omitempty"`
 	MaxTimestamp      uint64          `json:"maxTimestamp,omitempty"`
 	RevertingTxHashes []common.Hash   `json:"revertingTxHashes,omitempty"`
-	ReplacementUuid   string          `json:"replacementUuid,omitempty"`
+	ReplacementUuid   uuid.UUID       `json:"replacementUuid,omitempty"`
 }
 
 // MarshalJSON implements the [json.Marshaler].
