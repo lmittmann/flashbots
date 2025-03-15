50361431c485e96c549d513a2554ddbb9d383c9c
lmittmann <lmittmann@users.noreply.github.com>
2025-03-15 10:22:07 +0100

    fix typo + comment cleanup

--- call_bundle_test.go
+++ call_bundle_test.go
@@ -23,7 +23,7 @@ func TestCallBundle(t *testing.T) {
 				BundleHash:        w3.H("0x73b1e258c7a42fd0230b2fd05529c5d4b6fcb66c227783f8bece8aeacdd1db2e"),
 				CoinbaseDiff:      w3.I("20000000000126000"),
 				EthSentToCoinbase: w3.I("20000000000000000"),
-				GasFees:           w3.I("126000"), // XXX
+				GasFees:           w3.I("126000"),
 				StateBlockNumber:  w3.I("5221585"),
 				TotalGasUsed:      42000,
 				Results: []flashbots.CallBundleResult{
@@ -31,7 +31,7 @@ func TestCallBundle(t *testing.T) {
 						CoinbaseDiff:      w3.I("10000000000063000"),
 						EthSentToCoinbase: w3.I("10000000000000000"),
 						FromAddress:       w3.A("0x02A727155aeF8609c9f7F2179b2a1f560B39F5A0"),
-						GasFees:           w3.I("63000"), // XXX
+						GasFees:           w3.I("63000"),
 						GasPrice:          w3.I("476190476193"),
 						GasUsed:           21000,
 						ToAddress:         w3.APtr("0x73625f59CAdc5009Cb458B751b3E7b6b48C06f2C"),
